a8ac1a862ea143bb37df54bac157cc09fab79bfc
qmckearney <qmckearney@mail.pima.edu>
2025-02-19 19:56:26 -0700

    Deleted Hello,World! and added a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Quinn McKearney
+# My first Python lab in my CIS129 course
+# Deleted Hello, World! and added a short story
+print('There once was a boy with bright blue hair from Tucson.')
+print('All of the other children around him would tease him and ask him why his hair was blue.')
+print('Little did the other kids know, he lived on a blueberry farm and thats all he ever had to eat.')
